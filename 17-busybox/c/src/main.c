fb44dfc195e190ce2e2d86c20ddc23fcaf1b6dac
Patrick Flynn <flynn@nd.edu>
2025-09-06 13:55:04 -0400

    API-summary.txt & some work on 17-busybox

--- 17-busybox/c/src/main.c
+++ 17-busybox/c/src/main.c
@@ -13,6 +13,8 @@
 
 #include <stdio.h>
 #include <neo6502.h>
+#include <neo/system.h>
+#include <neo/graphics.h>
 
 // local-API-supoprt files (copy them here)
 // if these API updated get incorporated into the
@@ -20,17 +22,29 @@
 // #include <neo/mouse.h>
 // #include <neo/controller.h>
 // #include <neo/math.h>
-// Note: as of this writing, llvm-mos-sdk actually has an updated controller.h in main,
-// but not in the releases.
+// (and remove {mouse, controller, math}.{c,h}, and also updating
+// the Makefile to remove the .c files from the source list).
+// Note: as of this writing, llvm-mos-sdk actually has an updated
+// controller.h in main, but not in the releases.
 
 #include "mouse.h"
 #include "controller.h"
 #include "math.h"
 
 int main(void) {
+  uint8_t i,st;
 
-   printf("Hello, World!\n");
+  st = neo_mouse_test_mouse_present();
+  printf("Mouse present: %d\n",st);
 
-   return 0;
+  // thos doesn't seem to do anything on the emulator
+  for(i=0;i<255;i++) { 
+    neo_mouse_set_mouse_display_cursor(i);
+    st = neo_api_error();
+    printf("%d: neo_api_error() returned %d\n",i,st);
+    uint32_t fc = neo_graphics_frame_count();
+    while (neo_graphics_frame_count() < fc+30) /* wait*/ ;
+    }
+  return 0;
 
 }
