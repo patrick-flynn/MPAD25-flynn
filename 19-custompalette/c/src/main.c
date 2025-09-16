101b12df3ef59f44a950476c140fc85eb18a8b15
Patrick Flynn <flynn@nd.edu>
2025-09-16 09:29:19 -0400

    palette demos

---
+++ 19-custompalette/c/src/main.c
@@ -0,0 +1,39 @@
+/* 
+ * Title: 19-custompalette
+ * Author: P Flynn
+ * Date: 16-Sep-2025
+*/
+
+#include <stdio.h>
+#include <neo/graphics.h>
+#include <neo/console.h>
+
+#define SC_W 320
+#define SC_H 240
+
+#define GRID_W (SC_W/16)
+#define GRID_H (SC_H/16)
+
+#define TRUE (0==0)
+#define FALSE (0==1)
+
+int main(void) {
+	uint16_t i,j;
+	uint8_t color=0;
+
+    // define custom ramp palette
+    for(i=0;i<256;i++) neo_graphics_set_palette(i,0,i,0);
+
+    neo_graphics_set_solid_flag(TRUE);
+
+    for(i=0;i<16;i++) { // x coordinate
+      for(j=0;j<16;j++) { // y coordinate
+        color = i*16+j;
+        neo_graphics_set_color(color);
+        // x1, y1, x2, y2
+        neo_graphics_draw_rectangle(j*GRID_W,i*GRID_H,(j+1)*GRID_W -1, (i+1)*GRID_H -1);
+        }
+      }
+
+    while (1==1);
+    }
