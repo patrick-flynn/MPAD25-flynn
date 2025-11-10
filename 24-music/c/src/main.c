df5cdaa9bc644677207ffe9bed8a1d076870ddfd
Patrick Flynn <flynn@nd.edu>
2025-11-10 16:35:31 -0500

    24-music, better but still not good

--- 24-music/c/src/main.c
+++ 24-music/c/src/main.c
@@ -1,11 +1,12 @@
 /* 
-/ Title:
+/ Title: 24-music
 /
-/ Description:
+/ Description: play tunes on the Neo, using a primitive
+/ text description derived from musicxml by an external python script.
 /
-/ Author:
+/ Author: P Flynn
 /
-/ Created:
+/ Created: 11-7-2025
 / Last Updated:
 /
 / Modinfo:
@@ -20,31 +21,19 @@
 #include <neo/system.h>
 #include <neo/file.h>
 
-#define FN "SailOnBoys.csv"
+// #define FN "SailOnBoys.csv"
+//#define FN "the-deep-river-samba-traditionalish.csv"
+#define FN "super-mario-bros-main-theme.csv"
 
 typedef struct {
   uint16_t offset;
   uint16_t pitch;
-  uint16_t dur;
+  uint16_t end;
   } note_t;
 
 #define NMAX 1024
 note_t note[NMAX];
-uint16_t nn=0;
-
-// ticks for a note of a specified duration ("16th", "eighth"m etc.)
-// at a specified tempo in quarter-notes per minute
-uint16_t getdur(char *str,uint8_t qpm) {
-  uint16_t dur;
-  if (!strcmp(str,"16th")) dur = 6000/(qpm*16);
-  else if (!strcmp(str,"eighth")) dur = 6000/(qpm*8);
-  else if (!strcmp(str,"quarter")) dur = 6000/(qpm*4);
-  else if (!strcmp(str,"half")) dur = 6000/(qpm*2);
-  else if (!strcmp(str,"whole")) dur = 6000/(qpm*1);
-  else
-    printf("ERROR: unknown duration %s\n",str);
-  return dur;
-}
+uint16_t nn;
 
 typedef struct {
   char nstr[5];
@@ -64,18 +53,18 @@ notemap_t notemap[] = {
 {"D-2",0x0045}, {"D2",0x0049}, {"E-2",0x004E},
 {"E2",0x0052}, {"F2",0x0057}, {"G-2",0x005C},
 {"G2",0x0062}, {"A-2",0x0068}, {"A2",0x006E},
-{"B-2",0x0075}, {"B2",0x007B}, {"C3",0x0083},
+{"B-2",0x0075}, {"B2",0x007B}, {"C-3",0x007B}, {"C3",0x0083},
 {"D-3",0x008B}, {"D3",0x0093}, 
 {"E-3",0x009C}, {"E3",0x00A5}, {"F3",0x00AF},
-{"G-3",0x00B9}, {"G3",0x00C4}, {"A-3",0x00D0},
+{"F#3",0x00B9}, {"G-3",0x00B9}, {"G3",0x00C4}, {"A-3",0x00D0},
 {"A3",0x00DC}, {"B-3",0x00E9}, {"B3",0x00F7},
 {"C4",0x0106}, {"D-4",0x0115}, {"D4",0x0126},
 {"E-4",0x0137}, {"E4",0x014A}, {"F4",0x015D},
-{"G-4",0x0172}, {"G4",0x0188}, {"A-4",0x019F},
+{"G-4",0x0172}, {"G4",0x0188}, {"G#4",0x019F},{"A-4",0x019F},
 {"A4",0x01B8},  {"B-4",0x01D2},
-{"B4",0x01EE}, {"C5",0x020B}, {"D-5",0x022A},
-{"D5",0x024B}, {"E-5",0x026E}, {"E5",0x0293},
-{"F5",0x02BA},  {"G-5",0x02E4}, {"G5",0x0310},
+{"B4",0x01EE}, {"C5",0x020B}, {"C#5",0x022A},{"D-5",0x022A},
+{"D5",0x024B}, {"D#5",0x026E}, {"E-5",0x026E}, {"E5",0x0293},
+{"F5",0x02BA}, {"F#5",0x02E4}, {"G-5",0x02E4}, {"G5",0x0310},
 {"A-5",0x033F}, {"A5",0x0370}, 
 {"B-5",0x03A4}, {"B5",0x03DC}, {"C6",0x0417}, 
 {"D-6",0x0455}, {"D6",0x0497}, {"E-6",0x04DD},
@@ -111,47 +100,86 @@ uint16_t getnote(char *str) {
 }
 
 int main(void) {
-  
-  printf("%d sound channels.\n",neo_sound_channel_count());
+  nn=0;
+  //printf("%d sound channels.\n",neo_sound_channel_count());
 
   uint8_t chan=1;
   neo_file_open(chan,FN,NEO_FILE_READ);
 
-  printf("open.\n");
+  //printf("open.\n");
   while(1==1) {
-    char line[80],*ptr, *limit = ptr+80;;
-    char *ostr, *nstr, *dstr;
-    uint16_t offset, pitch, dur;
-    for(ptr=line; ; ptr++) {
+    char line[80],*ptr=line, *limit = ptr+80;;
+    char *ostr, *nstr, *estr;
+    uint16_t offset, pitch, end;
+    //printf("line %p\n",line);
+    for( ; ; ptr++) {
       neo_file_read(chan,ptr,1);
-      printf("read %c\n",*ptr);
-      if (neo_file_eof(chan)) {printf("eof.\n"); break;}
-      if (*ptr == '\n') {printf("newline.\n"); break;}
-      if (*ptr == limit) {printf("limit.\n"); break;}
-      printf("not broke.\n");
+      //printf("ptr %p read '%c' [0x%x]\n",*ptr,*ptr);
+      if (neo_file_eof(chan)) break;
+      if (*ptr == 0x0a) break;
+      if (ptr == limit) break;
       }
-    printf("broke\n");
     if (ptr == limit) {
       printf("ERROR: buffer overflow\n");
       exit(-1);
       };
-    printf("bla\n");
-    printf("read [%s]\n",line);
-    printf("bla2\n");
-    exit(-1);
+    if (neo_file_eof(chan)) {
+      neo_file_close(chan);
+      break;
+      }
+    // clobber newline
+    *ptr = '\0';
+    //printf("read [%s]\n",line);
+    //exit(-1);
     // we have a line in str. chop it into pieces
     ostr = strtok(line,",");
     // convert to int
-    offset = atoi(ostr);
+    //printf("ostr %s\n",ostr);
+    offset = atoi(ostr)/3;
+    //printf("offset %d\n",offset);
+    nstr = strtok(NULL,",");
+    //printf("nstr %s\n",nstr);
     pitch = getnote(nstr); // API pitch
-    dur = getdur(dstr,120); // duration in ticks at specified qpm 
+    //printf("pitch 0x%x\n",pitch);
+    estr = strtok(NULL,",");
+    //printf("dstr %s\n",dstr);
+    end = atoi(estr)/3; // duration in ticks at specified qpm 
     note[nn].offset = offset;
     note[nn].pitch = pitch;
-    note[nn++].dur = dur;
+    note[nn].end = end;
+    //printf("read note %d: [%d %p %d]\n",nn,note[nn].offset,note[nn].pitch,note[nn].end);
+    nn++;
     if (nn== NMAX) {
       printf("Note buffer too small.\n");
       exit(-1);
       }
     }
-  exit(0);
+  //printf("play (%d notes).\n",nn);
+
+  // now play the tune
+  uint32_t t0 = neo_system_timer() + 10; // 0.1 second delay
+  uint16_t index = 0;
+  while (1==1) {
+    uint8_t ch=0;
+    uint32_t off = note[index].offset;
+    //printf("offset %d: ",off);
+    // wait for timer to reach ofset for current note
+    while (neo_system_timer() < t0 + off) /* wait */;
+    // play all notes with this offset
+    while (note[index].offset == off) {
+      // play up to channel_count sounds
+      if (ch < neo_sound_channel_count()) {
+        uint32_t tdur = note[index].end - off;
+        uint16_t dur=tdur;
+        //printf("[%d:%p %d]",index,note[index].pitch,dur);
+        neo_sound_queue(ch,note[index].pitch,dur,0,0);
+        }
+      ch++;
+      index++;
+      if (index >= nn) break;
+      }
+    //printf("\n");
+    if (index >= nn) break;
+    }
+  while (1==1) ;
 }
--- 24-music/c/xml/parse.py
+++ 24-music/c/xml/parse.py
@@ -4,8 +4,11 @@ import music21
 
 # get some music.
 
-fn = 'Sail On, Boys v2.musicxml'
-score = music21.converter.parse(fn)
+#fn = 'Sail On, Boys v2.musicxml'
+#fn = 'the-deep-river-samba-traditionalish.musicxml'
+fn = 'super-mario-bros-main-theme.musicxml'
+
+score = music21.converter.parse(fn).expandRepeats()
 
 # This score was arranged for clarinet choir (Bb, Alto, Bass, Contra-alto, Contrabass),
 # so all of the parts need to be transposed.
@@ -24,7 +27,12 @@ tr = { 'Bb Clarinet': music21.interval.Interval('M-2'),
        'Eb Alto': music21.interval.Interval('M-6'),
        'Bb Bass': music21.interval.Interval('M-9'),
        'Eb Contra-alto': music21.interval.Interval('M-13'),
-       'Bb Contrabass': music21.interval.Interval('M-16')
+       'Bb Contrabass': music21.interval.Interval('M-16'),
+       'Soprano Saxophone': music21.interval.Interval('M-2'),
+       'Alto Saxophone': music21.interval.Interval('M-6'),
+       'Tenor Saxophone': music21.interval.Interval('M-9'),
+       'Baritone Saxophone': music21.interval.Interval('M-13'),
+       'Piano': 0
      }
 
 parts = score.parts
@@ -34,7 +42,11 @@ notes = []
 
 for pno,part in enumerate(parts):
   inst = part.getInstrument()
-  iname = ' '.join(inst.partName.split(' ')[:2])
+  iname = inst.partName.split(' ')
+  if len(iname) >= 2:
+    iname = ' '.join(iname.split(' ')[:2])
+  else:
+    iname = iname[0]
   # Eb instruments not properly transposed
   print(f'Inst: {iname}; transposition: {tr[iname]}')
   for el in part.flatten():
@@ -42,24 +54,15 @@ for pno,part in enumerate(parts):
     if isinstance(el,music21.note.Note):
       #print(f' Note: offset {el.offset}, pitch {el.nameWithOctave} duration {el.duration}')
       # fix the *(@&$# note
-      el.transpose(tr[iname],inPlace=True) # whee!
+      if tr[iname] != 0:
+        el.transpose(tr[iname],inPlace=True) # whee!
+      # enter start and end times (numerical)
+      el.nstart = 100.0 * float(el.offset)
+      el.nend = 100.0 * float(el.offset + el.duration.quarterLength)
       notes.append(el)
       #print(f'{float(el.offset)},{el.nameWithOctave},{el.duration.type}')
 
-
-#sort by offset
 notes.sort(key = lambda x: float(x.offset))
 
 for n in notes:
-  print(f'{int(100*float(n.offset))},{n.nameWithOctave},{n.duration.type}')
-        
-
-
-
-
-  #for nrno,nor in enumerate(part.notesAndRests):
-  #  if isinstance(nor,note.Note):
- #     print(f'{pno},{nrno},note,{nor.nameWithOctave},{nor.duration.type}')
-#    else:
-#      print(f'{pno},{nrno},rest,rest,{nor.duration.type}')
-#
+  print(f'{int(n.nstart)},{n.nameWithOctave},{int(n.nend)}')
